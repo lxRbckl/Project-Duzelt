cdd1a5c203a0b59a433d0b066c9a48c84195537e
lxrbckl <lxrbckl@gmail.com>
2022-12-01 20:18:01 -0600

    introduction to V1

---
+++ main.py
@@ -0,0 +1,20 @@
+# import <
+from os import system
+from time import sleep
+
+# >
+
+
+# main <
+if (__name__ == '__main__'):
+
+    while (True):
+
+        # run patch <
+        # wait <
+        system(command = 'bash patch.sh')
+        sleep(150)
+
+        # >
+
+# >
